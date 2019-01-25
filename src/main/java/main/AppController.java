caa9733b74eb892fafe5c59afefa199bb6d47eb2
Joseph Baca <jrbaca@gmail.com>
2019-01-25 00:26:28 -0600

    Initial commit

---
+++ src/main/java/main/AppController.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+class AppController implements Initializable {
+
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+  }
+}
---
+++ src/test/java/main/TestTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class TestTest {
+
+  @Test
+  void testTest() {
+    assertTrue(true);
+  }
+
+}
